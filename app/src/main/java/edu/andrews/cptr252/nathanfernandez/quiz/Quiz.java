14d6cfa422d2a1a9f56d96bece4924fe45cfc363
natndez <fernandeznathan891@gmail.com>
2022-04-14 17:04:40 -0400

    Created Quiz.Java and coded some necessary methods.

---
+++ app/src/main/java/edu/andrews/cptr252/nathanfernandez/quiz/Quiz.java
@@ -0,0 +1,46 @@
+package edu.andrews.cptr252.nathanfernandez.quiz;
+
+import java.util.UUID;
+
+/**
+*   Manage information for any specified quiz question
+*/
+public class Quiz {
+    //Unique ID for question
+    private UUID mId;
+
+    //The question that the user will enter
+    private String mQuestion;
+
+    /**
+     * Create and initialize new question
+     */
+    public void Question() {
+        //Generate unique id for new question
+        mId = UUID.randomUUID();
+    }
+
+    /**
+     * Return unique Id for the question
+     * @return Question Id;
+     */
+    public UUID getId(){
+        return mId;
+    }
+
+    /**
+     * Return the question
+     * @return the question
+     */
+    public String getQuestion() {
+        return mQuestion;
+    }
+
+    /**
+     * Provide the question
+     * @param question New Question
+     */
+    public void setQuestion(String question){
+        mQuestion = question;
+    }
+}
